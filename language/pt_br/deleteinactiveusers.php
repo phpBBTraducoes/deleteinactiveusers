86235f94708973ad35e114274de503a7910c33b8
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-08-09 12:02:11 -0300

    ADD

---
+++ language/pt_br/deleteinactiveusers.php
@@ -0,0 +1,43 @@
+<?php
+/**
+*
+* @package phpBB Extension - Delete Inactive Users
+* @copyright (c) 2019 dmzx - https://www.dmzx-web.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Brazilian Portuguese translation by eunaumtenhoid [2019][ver 1.0.1] (https://github.com/phpBBTraducoes)
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, [
+	'DELETE_INACTIVE_USERS_MESSAGE_REG'		=> 'Contas com <strong>%1$s</strong> post serão apagadas após <strong>%2$s</strong>.<br />Mantenha-se ativo neste fórum e post no mínimo <strong>%3$s</strong> para que sua conta não seja excluída.',
+	'DELETE_INACTIVE_USERS_TOPIC'	=> [
+		1 => '%1$s tópico',
+		2 => '%1$s tópicos',
+	],
+	'DELETE_INACTIVE_USERS_MESSAGE_RETURN'	=> '%sRetornar para a página ínicial%s',
+]);
